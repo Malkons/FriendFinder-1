6363a0ea95f2f6804a8ccf89ffdc9102a1713649
apleek3 <apleek3@gmail.com>
2018-09-05 17:33:32 -0700

    basic friends added to friendArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,13 +3,30 @@
 // ===============================================================================
 
 var friendArray = [
-    {
-      name: "John Smith",
-          photo: "picture",
-          scores: []
-    }
-  ];
-  
-  // Export the array making accessible to the user if they want to know about other people. 
-  module.exports = friendArray;
-  
+  {
+    name: "John Smith",
+    photo: "https://cdn.history.com/sites/2/2013/12/john-smith-52909750-H.jpeg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  }, {
+    name: "Pocahantas",
+    photo: "https://gdb.voanews.com/E00A4A41-6654-480D-8284-933268C309BB_cx20_cy36_cw59_w1023_r1_s.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  }, {
+    name: "Guy",
+    photo: "https://cook.fnr.sndimg.com/content/dam/images/cook/editorial/talent/guy-fieri/CCTV-TalentAvatar-Guy-Fieri-800x800.jpg.rend.hgtvcom.616.616.suffix/1478703698614.jpeg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  }, {
+    name: "Alex Lee",
+    photo: "https://www.homeworldbusiness.com/wp-content/uploads/2016/10/Oxo_Lee.jpg",
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  }, {
+    name: "Walt Disney",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Walt_Disney_1946.JPG/220px-Walt_Disney_1946.JPG",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  }
+
+
+];
+
+// Export the array making accessible to the user if they want to know about other people. 
+module.exports = friendArray;
